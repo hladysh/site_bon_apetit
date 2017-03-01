4ec5ecac754fabf750bddd8aa1bbc33431ef72e5
Ivan <hladysh.im@gmail.com>
2017-03-01 10:07:57 +0000

    add media to mein page

--- script/main.js
+++ script/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-	$(function() {
-		var url = window.location.pathname,
-			urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
-		$('.nav_content li a').each(function() {
-			if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-				$(".nav_content li ").removeClass('active');
-				$(this).addClass('active');
+	$(function () {
+	    var url = window.location.pathname,
+	        urlRegExp = new RegExp(url.replace(/\/$/, '') + "$");
+	    $('.nav_content li a').each(function () {
+	        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+	            $(".nav_content li ").removeClass('active');
+	            $(this).addClass('active');
 
-			}
-		});
+	        }
+	    });    
 
-		$(".tab_item").not(":first").hide();
-		$(".menu_food .tab").click(function(e) {
-			e.preventDefault();
-			$(".menu_food .tab").removeClass("active").eq($(this).index()).addClass("active");
-			$(".tab_item").hide().eq($(this).index()).fadeIn();
-		}).eq(0).addClass("active");
+	$(".tab_item").not(":first").hide();
+	$(".menu_food .tab").click(function(e) {
+		e.preventDefault();
+		$(".menu_food .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab_item").hide().eq($(this).index()).fadeIn();
+	}).eq(0).addClass("active");
 
 
-	});
 });
+		});
